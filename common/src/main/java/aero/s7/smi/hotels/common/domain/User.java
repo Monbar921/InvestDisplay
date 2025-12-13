35a9e7653222bfa2264731d9ddf3257cefc06027
Monbar921 <yqemahaty@gmail.com>
2025-12-13 17:34:59 +0700

    XXX- Add controller

---
+++ common/src/main/java/aero/s7/smi/hotels/common/domain/User.java
@@ -0,0 +1,25 @@
+package aero.s7.smi.hotels.common.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
